de9427514050c139dc640007bc6d41be55f3c4c2
code4jazz <joko.h.prasetyo@gmail.com>
2021-02-08 21:14:28 +0700

    Add files via upload

---
+++ studioblox_bot.js
@@ -0,0 +1,30 @@
+class StudioBloxTelegram implements ScratchExtension {
+	getInfo(): ExtensionMetadata {
+		return {
+			id: "StudioBloxTelegram",
+			name: "Telegram Bot",
+			blocks: [{
+				opcode: "sendMsg",
+				blockType: Scratch.BlockType.COMMAND,
+				text: "SendTo [ChatID] [text]",
+				arguments: {
+					ChatID: {
+						type: Scratch.ArgumentType.STRING,
+						defaultValue: "TelegramChatID"
+					},
+					text: {
+						type: Scratch.ArgumentType.STRING,
+						defaultValue: "Hi..Chat with SB-Lion here..."
+					}
+				}
+			}]
+		}
+	}
+	async sendMsg({ChatID, text}: {ChatID: string, text: string}) {
+		const Http = new XMLHttpRequest();
+		const url='https://api.telegram.org/bot1123142071:AAGu6LFpa9NpM_yKnLsrv2XMh5mODUjLhjY/sendMessage?chat_id='+ ChatID + '&text=' + text;
+		Http.open("POST", url);
+		Http.send();		
+	}
+}
+Scratch.extensions.register(new StudioBloxTelegram())
