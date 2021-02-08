1f17f2df615389222e13c40547a43a501ecce51d
code4jazz <joko.h.prasetyo@gmail.com>
2021-02-08 22:44:40 +0700

    Update studioblox_bot.js

--- studioblox_bot.js
+++ studioblox_bot.js
@@ -26,22 +26,20 @@ class TelegramBot {
 	}
 
 	sendMsg({ChatID, text}) {
-	let data = {
-			'chat_id' : ChatID,
-			'text' : text,
+	var data = {
+			"chat_id" : ChatID,
+			"text" : text,
 		}
 
-	return	fetch('https://api.telegram.org/bot1123142071:AAGu6LFpa9NpM_yKnLsrv2XMh5mODUjLhjY/sendMessage',{
-					method : 'POST',
-					headers : {
-						'Content-type' : 'application/json'
+	return	fetch("https://api.telegram.org/bot1123142071:AAGu6LFpa9NpM_yKnLsrv2XMh5mODUjLhjY/sendMessage",{
+					"method" : "POST",
+					"headers" : {
+						"Content-type" : "application/json"
 					},
-					body : JSON.stringify(data)			
+					"body" : JSON.stringify(data)			
 				}
 			)
-			.then(res => res.text())
-			.then(teks => console.log(teks))
-			.catch(err => console.log(err))
+			.then(response => response.text())
 	}
 
 }
