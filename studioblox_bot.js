cd1112ffe6b8566a03f751c909ca50883f889dad
code4jazz <joko.h.prasetyo@gmail.com>
2021-02-11 22:21:54 +0700

    Update studioblox_bot.js

--- studioblox_bot.js
+++ studioblox_bot.js
@@ -1,13 +1,18 @@
-class TelegramBot {
-	constructor() {
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+const Cast = require('../../util/cast');
+const log = require('../../util/log');
+
+
+class Scratch3TelegramBlox {
+	constructor(runtime) {
+		this.runtime = runtime;
     }
 
 	getInfo() {
 		return {
-			"id": "StudioBloxTelegram",
+			"id": "telegramblox",
 			"name": "Telegram Blox",
-			"iconURI" : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DE" +
-            "UIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==",
 			"blocks": [
 			{
 				"opcode": "sendMsg",
@@ -57,14 +62,12 @@ class TelegramBot {
 	}
 
 	readMsg({ChatID, teks}) {
-	return fetch("https://script.google.com/macros/s/AKfycbxtA-vR75ljZKLcwn5p21HYNuBL8fZjWL4Eot1iDu6gg9QVhog/exec?jenis=read",{
+	return  fetch("https://script.google.com/macros/s/AKfycbxtA-vR75ljZKLcwn5p21HYNuBL8fZjWL4Eot1iDu6gg9QVhog/exec?jenis=read",{
 					"method" : "GET"		
 				}
 			)
 			.then(response => response.text())
-	//var data2 = JSON.parse(data1)		
-	//return data2
 	}
 
 }
-Scratch.extensions.register(new TelegramBot())
+Scratch.extensions.register(new Scratch3TelegramBlox())
